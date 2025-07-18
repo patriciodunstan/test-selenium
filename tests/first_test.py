ee64fdad2c7daa1dcde8047e46ddf23f29c6497a
Patricio Dunstan <pdunstan@nexia.cl>
2025-07-17 21:50:46 -0400

    Actualizar la inicialización del navegador en las pruebas de Selenium para usar el servicio de Chrome

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -8,8 +9,8 @@ def test_page_title():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    
-    browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     browser.get("https://www.github.com")
 
     titleElement = browser.find_element(
